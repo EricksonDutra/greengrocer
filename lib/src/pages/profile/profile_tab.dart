c67685ea862a79777414736f61ec441cf46984c6
EricksonDutra <ericksond10@gmail.com>
2024-02-02 22:03:39 -0400

    GETx - Show form data

--- lib/src/pages/profile/profile_tab.dart
+++ lib/src/pages/profile/profile_tab.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:greengrocer/src/pages/auth/controller/auth_controller.dart';
 import 'package:greengrocer/src/pages/common_widgets/input_field_widget.dart';
+import 'package:greengrocer/src/services/validators.dart';
 import 'package:greengrocer/src/config/app_data.dart' as appData;
 
 class ProfileTab extends StatefulWidget {
@@ -41,26 +42,26 @@ class _ProfileTabState extends State<ProfileTab> {
           InputFieldWidget(
             icon: const Icon(Icons.email),
             label: 'E-mail',
-            initialValue: appData.user.email,
+            initialValue: authController.user.email,
             readOnly: true,
           ),
           InputFieldWidget(
             icon: const Icon(Icons.person),
             label: 'Nome',
-            initialValue: appData.user.name,
+            initialValue: authController.user.name,
             readOnly: true,
           ),
           InputFieldWidget(
             icon: const Icon(Icons.phone),
             label: 'Celular',
-            initialValue: appData.user.phone,
+            initialValue: authController.user.phone,
             readOnly: true,
           ),
           InputFieldWidget(
             icon: const Icon(Icons.file_copy),
             label: 'CPF',
             isSecret: true,
-            initialValue: appData.user.cpf,
+            initialValue: authController.user.cpf,
             readOnly: true,
           ),
           SizedBox(
@@ -82,6 +83,8 @@ class _ProfileTabState extends State<ProfileTab> {
   }
 
   Future<bool?> _updatePassword() {
+    final passwordController = TextEditingController();
+    final _formKey = GlobalKey<FormState>();
     return showDialog(
       context: context,
       builder: (bc) => Dialog(
@@ -89,44 +92,68 @@ class _ProfileTabState extends State<ProfileTab> {
           children: [
             Padding(
               padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 12),
-                    child: Text(
-                      'Atualizar senha',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 12),
+                      child: Text(
+                        'Atualizar senha',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
-                  ),
-                  const InputFieldWidget(
-                    isSecret: true,
-                    icon: Icon(Icons.lock),
-                    label: 'Senha atual',
-                  ),
-                  const InputFieldWidget(
-                    isSecret: true,
-                    icon: Icon(Icons.lock_outline),
-                    label: 'Nova senha',
-                  ),
-                  const InputFieldWidget(
-                    isSecret: true,
-                    icon: Icon(Icons.lock_outline),
-                    label: 'Confirmar nova senha',
-                  ),
-                  SizedBox(
-                    height: 45,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: const Text('Atualizar'),
+                    const InputFieldWidget(
+                      isSecret: true,
+                      icon: Icon(Icons.lock),
+                      label: 'Senha atual',
+                      validator: passwordValidador,
                     ),
-                  ),
-                ],
+                    const InputFieldWidget(
+                      isSecret: true,
+                      icon: Icon(Icons.lock_outline),
+                      label: 'Nova senha',
+                      validator: passwordValidador,
+                    ),
+                    InputFieldWidget(
+                      isSecret: true,
+                      icon: Icon(Icons.lock_outline),
+                      label: 'Confirmar nova senha',
+                      validator: (password) {
+                        final result = passwordValidador(password);
+
+                        if (result != null) {
+                          return result;
+                        }
+
+                        if (password != passwordController.text) {
+                          return 'As senhas não são equivalentes';
+                        }
+
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 45,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        )),
+                        onPressed: () {
+                          _formKey.currentState!.validate();
+                        },
+                        child: const Text('Atualizar'),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
             Positioned(
